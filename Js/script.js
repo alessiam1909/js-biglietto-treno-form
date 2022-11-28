48ea9a8f3a5dba55fe2c6ecafa49c700b611c886
alessiam1909 <ale.menon.vr@gmail.com>
2022-11-28 17:54:28 +0100

    inizio creazione form stilizzata

---
+++ Js/script.js
@@ -0,0 +1,5 @@
+// Stampo in console log il nome e cognome inserito dall'utente
+
+let nomeCognome = document.getElementById('nome-cognome');
+let valueNomeCognome = nomeCognome.value;
+console.log(valueNomeCognome);
